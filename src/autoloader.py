c9be1661194b77e9fb8054d1d6c57815196708ae
AshIsbitt <ashisbitt@icloud.com>
2021-09-19 22:29:29 +0100

    Creating and adding to .json

--- src/autoloader.py
+++ src/autoloader.py
@@ -1,13 +1,50 @@
 import sys
+import os
+import json
+
+SAVED_FILE = 'autoloader.json'
 
 # Function to create JSON file
-
+def createJSONfile():
+    jsonFile = open(SAVED_FILE, 'x')
+    return jsonFile
 
 # Function to write to JSON
+def addItemToJSON():
+    try:
+        with open(SAVED_FILE, 'rb') as jsonFile:
+            jsonRaw = json.load(jsonFile)
+    except FileNotFoundError:
+        jsonRaw = json.load(createJSONfile())
 
+    print('Enter type of location:')
+    print("'web': Website location")
+    print("'app': Local software to run")
+    print("'dir': Directory location to open in file explorer")
 
+    newLocType = input('>>> ')
+
+    if newLocType.lower() not in ['web', 'app', 'dir']:
+        print('Try again\n')
+        addItemToJSON()
+
+    print("Enter location (If 'app' or 'dir', make sure you use absolute locations)")
+    newLoc = input(">>> ")
+
+    jsonEntry = {f"{newLocType}": f"{newLoc}"}
+
+    jsonRaw.append(jsonEntry)
+
+    with open(SAVED_FILE, 'w') as jsonFile:
+        json.dump(jsonRaw)
 # Function to read/print out JSON
+def viewJSONDoc():
+    jsonRaw = json.loads(SAVED_FILE)
 
+    for key, value in jsonRaw:
+        print(f'({key}), {value}')
+
+    print('\n')
 
 # Function to open all listed locations in JSON
 
@@ -20,9 +57,9 @@ def main():
 
     try:
         if entry.lower() == 'r':
-            run()
+            pass
         elif entry.lower() == 'm':
-            print('/nManage saved locations')
+            print('\nManage saved locations')
             print("'v': View saved locations")
             print("'a': Add new location")
             print("'r': Remove saved location")
@@ -31,9 +68,9 @@ def main():
             manageEntry = input(">>> ")
 
             if manageEntry.lower() == 'v':
-                pass
+                viewJSONDoc()
             elif manageEntry.lower() == 'a':
-                pass
+                addItemToJSON()
             elif manageEntry.lower() == 'r':
                 pass
             elif manageEntry.lower() == 'e':
@@ -45,9 +82,8 @@ def main():
             print('Goodbye')
             sys.exit
     except:
-        print("Error, unknown command")
-        sys.exit
-
+        print('Error')
+        sys.exit()
 
 if __name__ == '__main__':
     main()

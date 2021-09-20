79a3d9af0176044d5d18a8289fb8128ab453ec0f
AshIsbitt <ashisbitt@icloud.com>
2021-09-20 15:51:36 +0100

    createJSONFile function and modifications to adding and viewing files

--- src/autoloader.py
+++ src/autoloader.py
@@ -6,29 +6,57 @@ import pyinputplus as pyplus
 SAVED_FILE = 'autoloader.json'
 
 # Run function to open all stored location
-
-
 def runAutoLoad():
     pass
 
 # function to read JSON and print all options to screen
 def viewJSONDoc():
-    with open(SAVED_FILE) as jsonFile:
+    with open(SAVED_FILE, 'r') as jsonFile:
         jsonData = json.load(jsonFile)
 
     for key, value in jsonData:
-        pass
+        print(key, value)
+
 # Function to append new item to JSON
 def addNewEntry():
-    pass
+    with open(SAVED_FILE, 'r') as jsonFile:
+        jsonData = json.load(jsonFile)
+        print(jsonData)
+
+
+    print('Enter type of location:')
+    print("'web': Website location")
+    print("'app': Local software to run")
+    print("'dir': Directory location to open in file explorer")
+
+    newLocType = input('>>> ')
+
+    if newLocType.lower() not in ['web', 'app', 'dir']:
+        print('Try again\n')
+        addItemToJSON()
+
+    print("Enter location (If 'app' or 'dir', make sure you use absolute locations)")
+    newLoc = input(">>> ")
+
+    # Add item to list in JSON Dict
+    jsonData[newLocType].append(newLoc)
+
+    with open(SAVED_FILE, 'w') as jsonFile:
+        json.dump(jsonData, jsonFile)
+
+    print('Item added')
+
+    main()
 
 # Function to remove item from JSON
 def removeEntry():
     pass
 
+
 # Function to delete JSON page
 def deleteJSON():
-    verifyFunc = pyplus.inputYesNo('Are you sure you want to delete the config file? ')
+    verifyFunc = pyplus.inputYesNo(
+        'Are you sure you want to delete the config file? ')
 
     if verifyFunc == 'yes':
         print('Deleting file...')
@@ -36,14 +64,26 @@ def deleteJSON():
 
     sys.exit()
 
+
+
+# Create new JSON
+def createJSONFile():
+    datadict = {}
+    datadict['web'] = []
+    datadict['app'] = []
+    datadict['dir'] = []
+
+    with open(SAVED_DATA, 'w') as JsonFile:
+        json.dump(datadict, JsonFile)
+
+
 # Function for dashboard to give users input options
 def main():
     print('AUTOLOADER')
 
     if not os.path.isfile(SAVED_FILE):
-        createdFile = open(SAVED_FILE, 'x')
+        createJSONFile()
         print('Initialising new document')
-
 
     print("Enter 'r' to run, or 'm' to manage stored locations. Enter 'x' to exit")
     entry = input(">>> ")
@@ -74,8 +114,9 @@ def main():
         elif entry.lower() == 'x':
             print('Goodbye')
 
-    except:
-        print('Error')
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
     main()

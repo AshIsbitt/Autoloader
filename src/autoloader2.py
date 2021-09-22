0e8eec18335aff151facc2b7a87ce76691e9c542
AshIsbitt <ashisbitt@icloud.com>
2021-09-22 11:28:16 +0100

    Argparse set up with subparsers for commands

--- src/autoloader2.py
+++ src/autoloader2.py
@@ -4,12 +4,27 @@ from typing import Sequence
 from typing import Optional
 import json
 
+def runAutoLoad():
+    print('Running')
+
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers(dest='command')
 
-    # Run or manage
-    parser.add_argument('function', type=str,
-         help="Run or manage saved locations. Functions: ['run', 'view', 'add', 'rem', 'erase']")
+    # Single command parsers
+    runParser = subparser.add_parser('run')
+    viewParser = subparser.add_parser('view')
+    eraseParser = subparser.add_parser('erase')
+
+    # Parsers requiring arguments
+    addParser = subparser.add_parser('add')
+    remParser = subparser.add_parser('rem')
+
+    addParser.add_argument('-c', '--category', type=str, required=True)
+    addParser.add_argument('-l', '--location', type=str, required=True)
+
+    remParser.add_argument('-c', '--category', type=str, required=True)
+    remParser.add_argument('-l', '--location', type=str, required=True)
 
     # Get arguments given
     args = parser.parse_args(argv)
@@ -17,19 +32,18 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
     # print given arguments
     pp.pprint(vars(args))
 
-    if args.function.lower() == 'run':
-        return('running')
-    elif args.function.lower() == 'add':
-        return('adding')
-    elif args.function.lower() == 'rem':
+    if args.command == 'run':
+        runAutoLoad()
+    elif args.command == 'view':
         pass
-    elif args.function.lower() == 'erase':
+    elif args.command == 'erase':
         pass
-    elif args.function.lower() == 'view':
+    elif args.command == 'view':
         pass
-    else:
-        return('Invalid function. Run -h for help')
-
+    elif args.command == 'add':
+        pass
+    elif args.command == 'rem':
+        pass
 
 if __name__=='__main__':
     exit(main())

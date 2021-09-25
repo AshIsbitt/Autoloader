31fe86b42ec7e5674585df78ebbe1189bf7a350d
AshIsbitt <ashisbitt@icloud.com>
2021-09-25 13:53:44 +0100

    first unit test written

--- autoloader/test_al_main.py
+++ autoloader/test_al_main.py
@@ -2,12 +2,21 @@
 
 import unittest
 import al_main
+import os
+import json
 
 class TestAutoloader(unittest.TestCase):
 
     # Test JSON file creation
     def test_createJSONFile(self):
-        self.assertEquals()
+        self.assertTrue(os.path.exists('autoloader_config.json'))
+
+        # Open file to test if dictionary exists in file
+        with open('autoloader_config.json', 'r') as f:
+            self.assertEqual(f.read(1), '{')
+            self.assertEqual(str(f.read())[-1], '}')
+
+    # test each type of file being opened
 
 if __name__ == '__main__':
     unittest.main()

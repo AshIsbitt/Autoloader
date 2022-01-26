1999a937b948590501c26be1ab798ec69619d23c
Ttibsi <ashisbitt@icloud.com>
2022-01-26 21:59:10 +0000

    Fix errors from pre-commit (flake8/mypy)

--- autoloader/aload.py
+++ autoloader/aload.py
@@ -1,142 +1,177 @@
 #!/usr/bin/env python3
-
 import argparse
-import pprint as pp
-from typing import Sequence
-from typing import Optional
+import getpass as gp
 import json
 import os
-import pyinputplus as pyplus
-import webbrowser
 import platform
+import pprint as pp
 import subprocess
-import getpass as gp
+import webbrowser
+from typing import Optional
+from typing import Sequence
 
-SAVED_FILE = 'autoloader_config.json'
+import pyinputplus as pyplus  # type: ignore
+
+SAVED_FILE = "autoloader_config.json"
+
 
 def createJSONFile():
     dataDict = {}
-    dataDict['web'] = []
-    dataDict['app'] = []
-    dataDict['dir'] = []
+    dataDict["web"] = []
+    dataDict["app"] = []
+    dataDict["dir"] = []
 
-    with open(SAVED_FILE, 'w') as JsonFile:
+    with open(SAVED_FILE, "w") as JsonFile:
         json.dump(dataDict, JsonFile)
 
+
 def runAutoLoad():
-    with open(SAVED_FILE, 'r') as jsonFile:
+    with open(SAVED_FILE, "r") as jsonFile:
         jsonData = json.load(jsonFile)
 
-    for value in jsonData['web']:
+    for value in jsonData["web"]:
         print(value)
-        webbrowser.open(f'https://{value}')
+        webbrowser.open(f"https://{value}")
 
-    for value in jsonData['app']:
-        if str(platform.platform())[0:5] == 'macOS':
-            if value[-4:] != '.app':
-                value += '.app'
+    for value in jsonData["app"]:
+        if str(platform.platform())[0:5] == "macOS":
+            if value[-4:] != ".app":
+                value += ".app"
 
-            if os.path.exists(f'/System/Applications/{value}'):
-                subprocess.Popen(['open', f'/System/Applications/{value}'])
+            if os.path.exists(f"/System/Applications/{value}"):
+                subprocess.Popen(["open", f"/System/Applications/{value}"])
             else:
-                subprocess.Popen(['open', f'/Applications/{value}'])
+                subprocess.Popen(["open", f"/Applications/{value}"])
 
-        elif str(platform.platform())[0:6] == 'Windows':
+        elif str(platform.platform())[0:6] == "Windows":
             subprocess.Popen('explorer "' + value + '"')
         else:
-            print("Please submit the following message on the github repo: https://github.com/AshIsbitt/Autoloader/issues")
+            print(
+                "Please submit the following message on the github repo: https://github.com/AshIsbitt/Autoloader/issues"
+            )
             print(f"Operating system not supported: {platform.platform()}")
 
+    for value in jsonData["dir"]:
+        subprocess.call(["open", "-R", value])
 
-    for value in jsonData['dir']:
-        subprocess.call(['open', '-R', value])
 
 def viewJSONDoc():
-    with open(SAVED_FILE, 'r') as jsonFile:
+    with open(SAVED_FILE, "r") as jsonFile:
         jsonData = json.load(jsonFile)
 
     for key, value in jsonData.items():
-        print(f'{key}: {value}')
+        print(f"{key}: {value}")
 
     return jsonData.items()
+
 
 def eraseJSONFile():
     verify = pyplus.inputYesNo(
         """Are you sure you want to completely erase the config file.
-This action cannot be undone. """)
+This action cannot be undone. """
+    )
 
-    if verify == 'yes':
-        print('Deleting config...')
+    if verify == "yes":
+        print("Deleting config...")
         os.remove(SAVED_FILE)
 
+
 def addItemToJson(category, item):
-    with open(SAVED_FILE, 'r') as jsonFile:
+    with open(SAVED_FILE, "r") as jsonFile:
         jsonData = json.load(jsonFile)
 
     jsonData[category].append(item)
 
-    with open(SAVED_FILE, 'w') as jsonFile:
+    with open(SAVED_FILE, "w") as jsonFile:
         json.dump(jsonData, jsonFile)
 
-    return(f'{item} added to {category} category')
+    return f"{item} added to {category} category"
+
 
 def removeItemFromJson(category, item):
-    with open(SAVED_FILE, 'r') as jsonFile:
+    with open(SAVED_FILE, "r") as jsonFile:
         jsonData = json.load(jsonFile)
 
     jsonData[category].remove(item)
 
-    with open(SAVED_FILE, 'w') as jsonFile:
+    with open(SAVED_FILE, "w") as jsonFile:
         json.dump(jsonData, jsonFile)
 
-    return(f'{item} removed from {category} category')
+    return f"{item} removed from {category} category"
+
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     if not os.path.isfile(SAVED_FILE):
         createJSONFile()
-        print('Creating new config file')
+        print("Creating new config file")
 
     parser = argparse.ArgumentParser()
-    subparser = parser.add_subparsers(dest='command')
+    subparser = parser.add_subparsers(dest="command")
 
     # Single command parsers
-    runParser = subparser.add_parser('run', help='Execute all saved locations in Autoloader')
-    viewParser = subparser.add_parser('view', help='Display locations in config file')
-    eraseParser = subparser.add_parser('erase', help='Erase config file')
+    subparser.add_parser("run", help="Execute all saved locations in Autoloader")
+    subparser.add_parser("view", help="Display locations in config file")
+    subparser.add_parser("erase", help="Erase config file")
 
     # Parsers requiring arguments
-    addParser = subparser.add_parser('add', help='Add item to config')
-    remParser = subparser.add_parser('rem', help='Remove item from config')
+    addParser = subparser.add_parser("add", help="Add item to config")
+    remParser = subparser.add_parser("rem", help="Remove item from config")
 
-    addParser.add_argument('-c', '--category', type=str, required=True, help="Type of location to save. Options are 'web', 'app' 'dir'")
-    addParser.add_argument('-l', '--location', type=str, required=True, help='Location ie website or file location to add')
+    addParser.add_argument(
+        "-c",
+        "--category",
+        type=str,
+        required=True,
+        help="Type of location to save. Options are 'web', 'app' 'dir'",
+    )
+    addParser.add_argument(
+        "-l",
+        "--location",
+        type=str,
+        required=True,
+        help="Location ie website or file location to add",
+    )
 
-    remParser.add_argument('-c', '--category', type=str, required=True, help="Type of location to remove. Options are 'web', 'app' 'dir'")
-    remParser.add_argument('-l', '--location', type=str, required=True, help='Location ie website or file location to remove')
+    remParser.add_argument(
+        "-c",
+        "--category",
+        type=str,
+        required=True,
+        help="Type of location to remove. Options are 'web', 'app' 'dir'",
+    )
+    remParser.add_argument(
+        "-l",
+        "--location",
+        type=str,
+        required=True,
+        help="Location ie website or file location to remove",
+    )
 
     # Get arguments given
     args = parser.parse_args(argv)
 
     # print given arguments
-#    pp.pprint(vars(args))
+    #    pp.pprint(vars(args))
 
-    if args.command == 'run':
+    if args.command == "run":
         runAutoLoad()
-    elif args.command == 'view':
+    elif args.command == "view":
         viewJSONDoc()
-    elif args.command == 'erase':
+    elif args.command == "erase":
         eraseJSONFile()
-    elif args.command == 'add':
-        if args.category not in ['web', 'app', 'dir']:
+    elif args.command == "add":
+        if args.category not in ["web", "app", "dir"]:
             print("Invalid category. Options: 'web', 'app', 'dir'")
         else:
             print(addItemToJson(args.category, args.location))
-    elif args.command == 'rem':
-        if args.category not in ['web', 'app', 'dir']:
+    elif args.command == "rem":
+        if args.category not in ["web", "app", "dir"]:
             print("Invalid category. Options: 'web', 'app', 'dir'")
         else:
             print(removeItemFromJson(args.category, args.location))
 
-if __name__=='__main__':
-    exit(main())
+    return 0
 
+
+if __name__ == "__main__":
+    exit(main())
--- autoloader/test_al_main.py
+++ autoloader/test_al_main.py
@@ -1,55 +1,60 @@
 # Testing suite devised for al_main using unittest
-
-import unittest
-import unittest.mock
-import aload
-import os
-import json
 import io
+import json
+import os
 import sys
+import unittest.mock
+
+import aload
+
 
 class TestAutoloader(unittest.TestCase):
 
     # Test JSON file creation
     def test_createJSONFile(self):
-        self.assertTrue(os.path.exists('autoloader_config.json'))
+        self.assertTrue(os.path.exists("autoloader_config.json"))
 
         # Open file to test if dictionary exists in file
-        with open('autoloader_config.json', 'r') as f:
-            self.assertEqual(f.read(1), '{')
-            self.assertEqual(str(f.read())[-1], '}')
+        with open("autoloader_config.json", "r") as f:
+            self.assertEqual(f.read(1), "{")
+            self.assertEqual(str(f.read())[-1], "}")
 
     # Test viewing file
     def test_viewJSONDoc(self):
-#        mockedFunction = al_main.viewJSONDoc()
-#        mockedFunction = MagicMock()
-#        mockedFunction()
+        #        mockedFunction = al_main.viewJSONDoc()
+        #        mockedFunction = MagicMock()
+        #        mockedFunction()
 
-#        mockedFunction.assert_called()
+        #        mockedFunction.assert_called()
 
-        #https://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print
+        # https://stackoverflow.com/questions/33767627/python-write-unittest-for-console-print
         capturedOut = io.StringIO()
         sys.stdout = capturedOut
-        al_main.viewJSONDoc()
+        aload.viewJSONDoc()
         sys.stdout = sys.__stdout__
 
         self.assertEqual(len(capturedOut.getvalue().splitlines()), 3)
-        self.assertEqual(str(capturedOut.getvalue())[0:4], 'web:')
+        self.assertEqual(str(capturedOut.getvalue())[0:4], "web:")
 
     # test adding item to json via mocking
     def test_addItemToJSON(self):
-        with patch('builtins.open', new_callable=mock_open(read_data = "{'disabed':True}")) as mockedFile:
-            with open('autoloader_config.json') as fh:
-                print(fh.read())
+        # Not sure what this patch is doing here
+        # with patch(
+        #    "builtins.open", new_callable=mock_open(read_data="{'disabed':True}")
+        # ) as mockedFile:
+
+        with open("autoloader_config.json") as fh:
+            print(fh.read())
 
     # test removing item from json via mocking
     def test_removeItemFromJson(self):
         pass
 
     # Test main by testing various function calls within the func
-        # Test erasing file by checking function call from main()
+    # Test erasing file by checking function call from main()
     def test_main(self):
         pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
